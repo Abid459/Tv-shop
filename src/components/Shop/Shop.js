3538f7975c222fa3c90a88292e6b9b7710f66e81
ABID <abid45986@gmail.com>
2022-03-26 00:38:47 +0600

    components added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import Products from '../Products/Products';
+
+const Shop = () => {
+    const [products,setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
+    return (
+        <div>
+            <Products products = {products}></Products>
+        </div>
+    );
+};
+
+export default Shop;
